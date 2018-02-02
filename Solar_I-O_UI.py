2ed1b336b5e2372426e72fb53819ccf8e708543a
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-02-02 15:23:27 +0530

    Constructed Basic UI

--- Solar_I-O_UI.py
+++ Solar_I-O_UI.py
@@ -3,4 +3,35 @@ win =Tk()
 win.title('Solar I-O')
 win.geometry('1024x640')
 
+
+def donothing():
+    print("i am doing nothing")
+
+
+menu=Menu(win)
+win.config(menu=menu)
+
+#Title Bar Declaration
+submenu=Menu(menu)
+menu.add_cascade(label="File",menu=submenu)
+submenu.add_command(label="New Project",command=donothing)
+submenu.add_command(label="Save",command=donothing)
+submenu.add_separator()
+submenu.add_command(label="Exit",command=donothing)
+
+editmenu=Menu(menu)
+menu.add_cascade(label="Edit",menu=editmenu)
+editmenu.add_command(label="Redo",command=donothing)
+
+## Main Body
+ProjectHeading =Label(win,text="Solar-Panel IO Predictor",fg="black")
+ProjectHeading.config(font=("Calibri", 24))
+ProjectHeading.pack(fill=X)
+
+#Button Declaration
+button_1=Button(win,text="Choose File")
+button_1.pack()
+
+
+
 win.mainloop()
